d7a4108de2fda13f8e7ace90e170787243edb30f
good-singer <15732658216@163.com>
2019-09-23 21:18:16 +0800

    '图标区域逻辑实现'

--- src/views/home/components/Icons.vue
+++ src/views/home/components/Icons.vue
@@ -1,94 +1,120 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png" />
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div
+          class="icon"
+          v-for="item of page"
+          :key="item.id"
+        >
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '滑雪季'
+      }, {
+        id: '0003',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '泡温泉'
+      }, {
+        id: '0004',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '动植物园'
+      }, {
+        id: '0005',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '美食'
+      }, {
+        id: '0006',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: 'KTV'
+      }, {
+        id: '0007',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '景点门票'
+      }, {
+        id: '0008',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '滑雪季'
+      }, {
+        id: '0009',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '景点门票'
+      }, {
+        id: '0010',
+        imgUrl: 'https://p1.meituan.net/travelcube/7264ce9c25de2e464e3acd996fe8ad362803.png',
+        desc: '一日游'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~@/assets/styles/varibles.styl'
-  .icons
-    overflow: hidden
+  @import '~@/assets/styles/mixins.styl'
+  .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-    .icon
-      overflow:hidden
-      float: left
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      position: relative
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: .44rem
-        box-sizing: border-box
-        padding: .1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: .44rem
-        line-height: .44rem
-        text-align: center
-        color: $darkTextColor
+  .icon
+    overflow:hidden
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    position: relative
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: .44rem
+      line-height: .44rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
 </style>
