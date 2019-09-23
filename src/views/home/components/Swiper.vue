4e3af8234ee516282455d80fbae7d903d80158a7
good-singer <15732658216@163.com>
2019-09-23 14:20:03 +0800

    轮播图开发

---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1736974.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://y.gtimg.cn/music/common/upload/MUSIC_FOCUS/1736974.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet
+    background: rgba(255,255,255,.45) !important
+  .wrapper >>> .swiper-pagination-bullet-active
+      background: white !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 40%
+    background: #969696
+    .swiper-img
+      width: 100%
+</style>
